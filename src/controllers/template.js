881c9bb27cddc3b947e422aa39b0a8f05f3b504c
Tapmeet <tapmeet@gmail.com>
2020-09-17 00:03:12 +0530

    Video

--- src/controllers/template.js
+++ src/controllers/template.js
@@ -154,7 +154,12 @@ function videoTemplate1(templateBlock, req, res) {
             if (block.blockData.imageFour == '' && block.blockData.containerFour != '') {
                 videoCheck = 1;
             }
-            recordCall([container1, container2, container3, container4])
+            var datas = {
+                block: block,
+                file: process.env.APIURL + 'template/videos/server-generated.mp4'
+            }
+            addTextTovideo(datas, req, res)
+            //recordCall([container1, container2, container3, container4])
             function recordCall(inputs) {
                 inputs.forEach(input => {
                     command.addInput(input);
@@ -300,7 +305,7 @@ function videoTemplate1(templateBlock, req, res) {
                         {
                             filter: 'drawtext',
                             options: {
-                               // fontfile: 'https://fonts.gstatic.com/s/oswald/v35/TK3_WkUHHAIjg75cFRf3bXL8LICs13FvsUtiZTaR.woff2',
+                                // fontfile: 'https://fonts.gstatic.com/s/oswald/v35/TK3_WkUHHAIjg75cFRf3bXL8LICs13FvsUtiZTaR.woff2',
                                 text: datas.block.blockData.blocksubTitle,
                                 fontsize: datas.block.blockData.blocksubTitleFontsize,
                                 fontcolor: subtitleColor,
