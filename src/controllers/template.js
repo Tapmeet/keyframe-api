aa1222abbd34f36cb51ead0339aec1db5e3666ef
Tapmeet <tapmeet@gmail.com>
2020-09-17 12:43:34 +0530

    error resolved

--- src/controllers/template.js
+++ src/controllers/template.js
@@ -154,12 +154,7 @@ function videoTemplate1(templateBlock, req, res) {
             if (block.blockData.imageFour == '' && block.blockData.containerFour != '') {
                 videoCheck = 1;
             }
-            var datas = {
-                block: block,
-                file: process.env.APIURL + 'template/videos/server-generated.mp4'
-            }
-            setTimeout(function () { addTextTovideo(datas, req, res) }, 3000);
-            //recordCall([container1, container2, container3, container4])
+            recordCall([container1, container2, container3, container4])
             function recordCall(inputs) {
                 inputs.forEach(input => {
                     command.addInput(input);
@@ -184,7 +179,7 @@ function videoTemplate1(templateBlock, req, res) {
                                     block: block,
                                     file: process.env.APIURL + 'template/videos/server-generated.mp4'
                                 }
-                                setTimeout(function () { addTextTovideo(datas, req, res) }, 3000);
+                                addTextTovideo(datas, req, res)
                                 console.log("success");
                             }
                             else {
@@ -214,8 +209,7 @@ function videoTemplate1(templateBlock, req, res) {
                                     block: block,
                                     file: process.env.APIURL + 'template/videos/server-generated.mp4'
                                 }
-                                setTimeout(function () { addTextTovideo(datas, req, res) }, 3000);
-
+                                addTextTovideo(datas, req, res)
                                 console.log("success");
                             }
                             else {
@@ -227,126 +221,110 @@ function videoTemplate1(templateBlock, req, res) {
             }
 
             function addTextTovideo(datas, req, res) {
-                var command = ffmpeg();
-                command.addInput(datas.file)
-                command.addInput(datas.file)
-                command.addInput(datas.file)
-                command.addInput(datas.file)
-                // var titleColor = datas.block.blockData.titleColor;
-                // if (titleColor.lenth == '4') {
-                //     titleColor = titleColor.replaceAll("#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])", "#$1$1$2$2$3$3");
-                // }
-                // var subtitleColor = datas.block.blockData.subtitleColor;
-                // if (subtitleColor.lenth == '4') {
-                //     subtitleColor = subtitleColor.replaceAll("#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])", "#$1$1$2$2$3$3");
-                // }
-                // console.log(datas.file);
-                // commands.addInput(datas.file)
-                // commands
-                //     .complexFilter([
-                //         'scale=1080:720[rescaled]',
-                //         // {
-                //         //     filter: 'drawbox',
-                //         //     options: {
-                //         //         x: 0,
-                //         //         y: 0,
-                //         //         color: 'white',
-                //         //         t: 'fill',
-                //         //         enable: 'between(t,0,1)'
-                //         //     },
-                //         //     inputs: 'rescaled',
-                //         //     outputs: 'output1',
+                var commands = ffmpeg();
+                var titleColor = datas.block.blockData.titleColor;
+                if (titleColor.lenth == '4') {
+                    titleColor = titleColor.replaceAll("#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])", "#$1$1$2$2$3$3");
+                }
+                var subtitleColor = datas.block.blockData.subtitleColor;
+                if (subtitleColor.lenth == '4') {
+                    subtitleColor = subtitleColor.replaceAll("#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])", "#$1$1$2$2$3$3");
+                }
+                console.log(datas.file);
+                commands.addInput(datas.file)
+                commands
+                    .complexFilter([
+                        'scale=1080:720[rescaled]',
+                        {
+                            filter: 'drawbox',
+                            options: {
+                                x: 0,
+                                y: 0,
+                                color: 'white',
+                                t: 'fill',
+                                enable: 'between(t,0,1)'
+                            },
+                            inputs: 'rescaled',
+                            outputs: 'output1',
 
-                //         // },
-                //         // {
-                //         //     filter: 'drawbox',
-                //         //     options: {
-                //         //         x: '(w + 120)/2',
-                //         //         y: '(h + 220)/2',
-                //         //         height: 240,
-                //         //         width: 480,
-                //         //         color: 'white',
-                //         //         t: 'fill',
-                //         //         enable: 'between(t,1,6)'
-                //         //     },
-                //         //     inputs: 'output1',
-                //         //     outputs: 'output2'
-                //         // },
-                //         // {
-                //         //     filter: 'drawbox',
-                //         //     options: {
-                //         //         x: '(w + 40)/2',
-                //         //         y: '(h + 140)/2',
-                //         //         height: 280,
-                //         //         width: 520,
-                //         //         color: 'white',
-                //         //         t: '2',
-                //         //         enable: 'between(t,1,6)'
-                //         //     },
-                //         //     inputs: 'output2',
-                //         //     outputs: 'output3'
-                //         // },
-                //         {
-                //             filter: 'drawtext',
-                //             options: {
-                //                 //fontfile: 'https://fonts.gstatic.com/s/oswald/v35/TK3_WkUHHAIjg75cFRf3bXL8LICs13FvsUtiZTaR.woff2',
-                //                 text: datas.block.blockData.blockTitle,
-                //                 fontsize: datas.block.blockData.blocktitleFontsize,
-                //                 fontcolor: titleColor,
-                //                 line_spacing: "20",
-                //                 x: '(w-text_w)/2',
-                //                 y: '(h-text_h-50)/2',
-                //                 box: 1,
-                //                 boxcolor: 'white@0.0',
-                //                 boxborderw: "50",
-                //                 bordercolor: 'white',
-                //                 enable: 'between(t,1.1,10)'
+                        },
+                        {
+                            filter: 'drawbox',
+                            options: {
+                                x: '(w + 120)/2',
+                                y: '(h + 220)/2',
+                                height: 240,
+                                width: 480,
+                                color: 'white',
+                                t: 'fill',
+                                enable: 'between(t,1,6)'
+                            },
+                            inputs: 'output1',
+                            outputs: 'output2'
+                        },
+                        {
+                            filter: 'drawbox',
+                            options: {
+                                x: '(w + 40)/2',
+                                y: '(h + 140)/2',
+                                height: 280,
+                                width: 520,
+                                color: 'white',
+                                t: '2',
+                                enable: 'between(t,1,6)'
+                            },
+                            inputs: 'output2',
+                            outputs: 'output3'
+                        },
+                        {
+                            filter: 'drawtext',
+                            options: {
+                                //fontfile: 'https://fonts.gstatic.com/s/oswald/v35/TK3_WkUHHAIjg75cFRf3bXL8LICs13FvsUtiZTaR.woff2',
+                                text: datas.block.blockData.blockTitle,
+                                fontsize: datas.block.blockData.blocktitleFontsize,
+                                fontcolor: titleColor,
+                                line_spacing: "20",
+                                x: '(w-text_w)/2',
+                                y: '(h-text_h-50)/2',
+                                box: 1,
+                                boxcolor: 'white@0.0',
+                                boxborderw: "50",
+                                bordercolor: 'white',
+                                enable: 'between(t,1.1,10)'
 
-                //             },
-                //             inputs: 'rescaled',
-                //             outputs: 'output4'
+                            },
+                            inputs: 'output3',
+                            outputs: 'output4'
 
-                //         },
-                //         {
-                //             filter: 'drawtext',
-                //             options: {
-                //                // fontfile: 'https://fonts.gstatic.com/s/oswald/v35/TK3_WkUHHAIjg75cFRf3bXL8LICs13FvsUtiZTaR.woff2',
-                //                 text: datas.block.blockData.blocksubTitle,
-                //                 fontsize: datas.block.blockData.blocksubTitleFontsize,
-                //                 fontcolor: subtitleColor,
-                //                 x: '(w-text_w )/2',
-                //                 y: '(h-text_h + 50)/2',
-                //                 box: 1,
-                //                 boxcolor: 'white@0.0',
-                //                 boxborderw: "50",
-                //                 bordercolor: 'white',
-                //                 enable: 'between(t,2,10)',
-                //             },
-                //             inputs: 'output4',
-                //             outputs: 'output'
-                //         },
-                //     ], 'output')
-                .complexFilter('[0:v]  setpts=PTS-STARTPTS, scale=630:470,pad=640:480:5:5:white [a0];[1:v] setpts=PTS-STARTPTS, scale=630:470,pad=640:480:5:5:white [a1];[2:v] setpts=PTS-STARTPTS,  scale=630:470,pad=640:480:5:5:white [a2];[3:v] setpts=PTS-STARTPTS,  scale=630:470,pad=640:480:5:5:white [a3];[a0][a1][a2][a3]xstack=inputs=4:layout=0_0|0_h0|w0_0|w0_h0[out]')
-                    .addOption('-map', '[out]',)
+                        },
+                        {
+                            filter: 'drawtext',
+                            options: {
+                               // fontfile: 'https://fonts.gstatic.com/s/oswald/v35/TK3_WkUHHAIjg75cFRf3bXL8LICs13FvsUtiZTaR.woff2',
+                                text: datas.block.blockData.blocksubTitle,
+                                fontsize: datas.block.blockData.blocksubTitleFontsize,
+                                fontcolor: subtitleColor,
+                                x: '(w-text_w )/2',
+                                y: '(h-text_h + 50)/2',
+                                box: 1,
+                                boxcolor: 'white@0.0',
+                                boxborderw: "50",
+                                bordercolor: 'white',
+                                enable: 'between(t,2,10)',
+                            },
+                            inputs: 'output4',
+                            outputs: 'output'
+                        },
+                    ], 'output')
                     .addOption('-c:v', 'libx264')
-                    .save('./src/Assets/template/videos/server-generated.mp4')
+                    .save('./server-generated1.mp4')
                     .on('start', function (commandLine) {
                         console.log(commandLine);
                     })
                     .on("error", function (er) {
-                        res.status(200).json({ message: 'Video failed' });
                         console.log(er);
                         console.log("error occured: " + er.message);
                     })
-                    //.addOption('-c:v', 'libx264')
-                    // .save('./src/Assets/template/videos/server-generated1.mp4')
-                    // .on('start', function (commandLine) {
-                    //     console.log(commandLine);
-                    // })
-                    // .on("error", function (er) {
-                    //     console.log(er);
-                    //     console.log("error occured: " + er.message);
-                    // })
                     .on("end", function (commandLine) {
                         res.status(200).json({ message: 'Video failed', data: './server-generated1.mp4' });
                         console.log(commandLine);
