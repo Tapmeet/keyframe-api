05162b5819eeabb9c0a056bb384a0b34d91776f4
sandeepvic <sandeepvic@yahoo.com>
2021-12-06 14:09:44 +0530

    Working paykick start

--- src/controllers/IPN.js
+++ src/controllers/IPN.js
@@ -12,6 +12,8 @@ const Team = require('../models/team');
  */
 exports.index = async (req, res, next) => {
   console.log(req.body);
+  res.status(200).send('OK');
+  res.end();
   try {
    
   } catch (error) {
