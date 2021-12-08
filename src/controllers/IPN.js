43eb0a19e82935967ad34eb7cea9b64a8cb62293
sandeepvic <sandeepvic@yahoo.com>
2021-12-08 12:17:27 +0530

    Working paykick start

--- src/controllers/IPN.js
+++ src/controllers/IPN.js
@@ -3,7 +3,7 @@
 /* eslint-disable valid-jsdoc */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-trailing-spaces */
-const IpnTb = require('../models/IPN');
+const Ipn = require('../models/ipn');
 const User = require('../models/user');
 const paykickstartIPNValidator = require('paykickstart-ipn-validator');
 /**
@@ -28,7 +28,7 @@ exports.index = async (req, res, next) => {
     console.log('thress');
     console.log(user);
     const userid = user._id;
-    const newTeam = new IpnTb({...req.body, userId: userid});
+    const newTeam = new Ipn({...req.body, userId: userid});
     console.log(newTeam);
     res.status(200).send('OK');
     res.end();
